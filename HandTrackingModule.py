e0ad72f8140118f7a45d88b902f8e87013cc24a4
phanhien9720 <51913684+phanhien9720@users.noreply.github.com>
2021-09-26 09:32:08 +0700

    Add files via upload

--- HandTrackingModule.py
+++ HandTrackingModule.py
@@ -12,7 +12,7 @@ class handDetector():
         self.mpHands = mp.solutions.hands
         self.hands = self.mpHands.Hands(self.mode,self.maxHands,self.detectionCon,self.trackCon)
         self.mpDraw = mp.solutions.drawing_utils
-        self.tipIds = [4,8,12,16,20]
+
 
     def findHands(self,img,draw= True):
         imgRGB = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
@@ -21,23 +21,19 @@ class handDetector():
             for handLms in self.results.multi_hand_landmarks:
                 if draw:
                     self.mpDraw.draw_landmarks(img,handLms,self.mpHands.HAND_CONNECTIONS)
+        return  img
     def findPosition(self,img,handNo = 0,draw =True):
-        xList = []
-        yList = []
-        bbox = []
         lmList = []
         if self.results.multi_hand_landmarks:
             myHand = self.results.multi_hand_landmarks[handNo]
             for id,lm in enumerate(myHand.landmark):
-                h,w,c,_ = img.shape
-                cx,cy = int(lm.x*w),int(lm.y*h)
-                xList.append(cx)
-                yList.append(cy)
-                self.lmList.append([id,cx,cy])
+                h,w,c, = img.shape
+                cx,cy = int(lm.x * w),int(lm.y * h)
+                lmList.append([id,cx,cy])
                 if draw:
                     cv2.circle(img,(cx,cy),5,(255,0,255),cv2.FILLED)
-        return lmList     
-    
+        return lmList
+
 def main():
     pTime = 0 
     cTime = 0
@@ -47,12 +43,17 @@ def main():
         success,img = cap.read()
         img = detector.findHands(img)
         lmList = detector.findPosition(img)
-    cTime = time.time
-    fps = 1/(cTime-pTime)
-    pTime = cTime
-    cv2.putText(image,f'FPS:{int(fps)}',(20,70),cv2.FONT_HERSHEY_PLAIN,3,(0,196,255),2)
-    cv2.imshow("video",img)
-    cv2.waitKey()
+        img = cv2.cvtColor(cv2.flip(img,1),cv2.COLOR_BGR2RGB)
+        img.flags.writeable = False
+        img.flags.writeable = True
+        img = cv2.cvtColor(img,cv2.COLOR_RGB2BGR)
+        cTime = time.time()
+        fps = 1/(cTime-pTime)
+        pTime = cTime
+        cv2.putText(img,f'FPS:{int(fps)}',(10,70),cv2.FONT_HERSHEY_PLAIN,3,(255,0,255),3)
+        cv2.imshow("video",img)
+        if cv2.waitKey(5) & 0xFF == 27:
+            break
 
 if __name__ == "__main__":
     main()
